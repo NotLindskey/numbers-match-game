7ddee767773cff02fb99e83dce25363b5974f0de
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-26 20:44:36 -0500

    generate a random image

--- script.js
+++ script.js
@@ -83,4 +83,10 @@ const images = [
   },
 ];
 
+const randomNumber = Math.floor(Math.random() * images.length);
+const randomImageName = images[randomNumber].image_name;
+const imageContainer = document.getElementById('imageContainer');
 
+const image = document.createElement('img');
+image.src = `images/${randomImageName}`;
+imageContainer.appendChild(image);
